373fdc5a8dfa9d8a867b1ce09df94a648e6408de
utkarsh1311 <itsutkarsh123@gmail.com>
2023-09-13 20:48:33 +0530

    add search functionality

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,10 +7,9 @@ import PokemonDetail from "./PokemonDetail";
 import { defPokemon } from "../constants";
 
 const Home = () => {
-  //
-
   const [pokemons, setPokemons] = useState([]);
   const [selected, setSelected] = useState(defPokemon);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     const fetchPokemon = async () => {
@@ -19,8 +18,9 @@ const Home = () => {
         setPokemons(localPokemons);
         return;
       }
-      console.log("fetching...")
-      const list = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=20&offset=0");
+      const list = await fetch(
+        "https://pokeapi.co/api/v2/pokemon/?limit=20&offset=0"
+      );
       let data = await list.json();
       const pArr = data.results.map(async (res) => {
         const a = await fetch(res.url);
@@ -28,26 +28,96 @@ const Home = () => {
       });
 
       const pokeList = await Promise.all(pArr);
-      const finalList = pokeList.map(p => {
-        return (
-          {
-            id: p.id,
-            name: p.name,
-            sprites: p.sprites,
-            types: p.types,
-            abilities: p.abilities,
-            height: p.height,
-            weight: p.weight,
-            stats: p.stats
-          }
-        )
-      })
+      const finalList = pokeList.map((p) => {
+        return {
+          id: p.id,
+          name: p.name,
+          sprites: {
+            other: {
+              "official-artwork": {
+                front_default:
+                  p.sprites.other["official-artwork"].front_default,
+              },
+            },
+            versions: {
+              "generation-v": {
+                "black-white": {
+                  animated: {
+                    front_default:
+                      p.sprites.versions["generation-v"]["black-white"].animated
+                        .front_default,
+                  },
+                },
+              },
+            },
+          },
+          types: p.types,
+          abilities: p.abilities,
+          height: p.height,
+          weight: p.weight,
+          stats: p.stats,
+        };
+      });
       localStorage.setItem("pokemons", JSON.stringify(finalList));
       setPokemons(finalList);
     };
 
     fetchPokemon();
   }, []);
+
+  useEffect(() => {
+    const searchPokemon = setTimeout(async () => {
+      if (query.trim().length > 0) {
+        const searchedPokemon = pokemons.filter((p) =>
+          p.name.startsWith(query.toLowerCase())
+        );
+        if (searchedPokemon.length > 0) {
+          setSelected(searchedPokemon[0]);
+        } else {
+          const newPokemon = await fetch(
+            `https://pokeapi.co/api/v2/pokemon/${query.toLowerCase()}`
+          );
+          if (newPokemon.status === 404) {
+            setSelected(defPokemon);
+            return;
+          }
+
+          const data = await newPokemon.json();
+
+          const newPoke = {
+            id: data.id,
+            name: data.name,
+            sprites: {
+              other: {
+                "official-artwork": {
+                  front_default:
+                    data.sprites.other["official-artwork"].front_default,
+                },
+              },
+              versions: {
+                "generation-v": {
+                  "black-white": {
+                    animated: {
+                      front_default:
+                        data.sprites.versions["generation-v"]["black-white"]
+                          .animated.front_default,
+                    },
+                  },
+                },
+              },
+            },
+            types: data.types,
+            abilities: data.abilities,
+            height: data.height,
+            weight: data.weight,
+            stats: data.stats,
+          };
+          setSelected(newPoke);
+        }
+      }
+    }, 1000);
+    return () => clearTimeout(searchPokemon);
+  }, [query]);
 
   const handleClick = (e) => {
     let pokemonId = e.target.id;
@@ -61,7 +131,7 @@ const Home = () => {
       <div className="col-start-2 col-end-8">
         <Navbar />
         <div className="w-full grid grid-cols-6 mt-6 gap-6">
-          <Search />
+          <Search handleSearch={(e) => setQuery((prev) => e.target.value)} />
           <PokemonGrid handleClick={handleClick} pokemons={pokemons} />
           {pokemons && <PokemonDetail pokemon={selected} />}
         </div>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,10 @@
-import React from "react";
 import search from "../assets/search.png";
 
-
-const Search = () => {
+const Search = ({ handleSearch }) => {
   return (
     <div className="relative flex items-center col-start-1 col-end-5 group">
       <input
+        onChange={handleSearch}
         className="w-full px-6 py-4 rounded-lg outline-none focus:outline-none font-semibold text-gray-600 group-hover:shadow-lg"
         type="text"
         placeholder="Search your pokemon"
